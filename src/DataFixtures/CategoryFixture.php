0ff2b0ba463a86e12867602365b38c1f25095c2d
nahusenayElias <eliasars@yahoo.com>
2025-03-20 22:21:48 +0200

    feat: DataFixture add

--- src/DataFixtures/CategoryFixture.php
+++ src/DataFixtures/CategoryFixture.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Category;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +10,13 @@ class CategoryFixture extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $category1 = new Category();
+        $category1->setName('Electronics');
+        $manager->persist($category1);
+
+        $category2 = new Category();
+        $category2->setName('Clothing');
+        $manager->persist($category2);
 
         $manager->flush();
     }
