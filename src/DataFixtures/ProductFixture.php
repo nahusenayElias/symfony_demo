c80856eb1e8b1ef635eb8009611e3ed67d04bf66
nahusenayElias <eliasars@yahoo.com>
2025-03-20 22:22:41 +0200

    feat: DataFixture add

--- src/DataFixtures/ProductFixture.php
+++ src/DataFixtures/ProductFixture.php
@@ -2,6 +2,8 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Product;
+use App\Entity\Category;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +11,19 @@ class ProductFixture extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $category = $manager->getRepository(Category::class)->findOneBy(['name' => 'Electronics']);
+
+        $product1 = new Product();
+        $product1->setName('Laptop');
+        $product1->setPrice(999.99);
+        $product1->setCategory($category);
+        $manager->persist($product1);
+
+        $product2 = new Product();
+        $product2->setName('Smartphone');
+        $product2->setPrice(499.99);
+        $product2->setCategory($category);
+        $manager->persist($product2);
 
         $manager->flush();
     }
